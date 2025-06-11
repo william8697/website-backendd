8c4696f995c9f9095baec899c0180853416e3faf
william8697 <scottwilliamsfurnitures@gmail.com>
2025-06-11 17:52:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const UAParser = require('ua-parser-js');
 const crypto = require('crypto');
 const { Parser } = require('@json2csv/plainjs');  
 const { v4: uuidv4 } = require('uuid');
-const LOGO_URL = 'https://www.dropbox.com/scl/fi/uetnfwoe05357p3alf8bx/WhatsApp-Image-2025-06-11-at-07.49.46_3b634219.jpg?rlkey=fj3gr8kjgpp7chjv2fqb3b8qx&st=a2l1c7gm&dl=0';
+const LOGO_URL = 'https://www.dropbox.com/scl/fi/uetnfwoe05357p3alf8bx/WhatsApp-Image-2025-06-11-at-07.49.46_3b634219.jpg?rlkey=fj3gr8kjgpp7chjv2fqb3b8qx&raw=1';
 const app = express();
 const PORT = process.env.PORT || 5000;
 const JWT_SECRET = '17581758Na.%';
